4bc958cfb4700a10c8e09640eb7cf7ee2f1e41eb
TheZeyuanFeng <205569811+HJRAmateur@users.noreply.github.com>
2025-05-03 20:52:28 -0700

    Create branch for paper: Bridging Model Predictive Control and Deep  Learning for Scalable Reachability Analysis

--- README.md
+++ README.md
@@ -1,44 +1,40 @@
-# DeepReach: A Deep Learning Approach to High-Dimensional Reachability
-### [Project Page](http://people.eecs.berkeley.edu/~somil/index.html) | [Paper](https://arxiv.org/pdf/2011.02082.pdf)<br>
+# DeepReach_MPC
+ Bridging Model Predictive Control and Deep  Learning for Scalable Reachability Analysis
 
-Repository Maintainers<br>
+Authors: [Zeyuan Feng](https://thezeyuanfeng.github.io/), Le Qiu, [Somil Bansal](http://people.eecs.berkeley.edu/~somil/index.html)
+
+Acknowledgement: This repo is built on [DeepReach](https://github.com/smlbansal/deepreach). Thanks all the maintainers for the supports! <br>
 [Albert Lin](https://www.linkedin.com/in/albertkuilin/),
 [Zeyuan Feng](https://thezeyuanfeng.github.io/),
 [Javier Borquez](https://javierborquez.github.io/),
 [Somil Bansal](http://people.eecs.berkeley.edu/~somil/index.html)<br>
-University of Southern California
-
-Original Authors<br>
-[Somil Bansal](http://people.eecs.berkeley.edu/~somil/index.html),
-Claire Tomlin<br>
-University of California, Berkeley
-
-(Still to come...) The Safe and Intelligent Autonomy (SIA) Lab at the University of Southern California
-is still working on an easy-to-use DeepReach Python package which will follow much of the same organizational principles as
-the [hj_reachability package in JAX](https://github.com/StanfordASL/hj_reachability) from the Autonomous Systems Lab at Stanford.
-The future version will include the newest tips and tricks of DeepReach developed by SIA.
-
-(In the meantime...) This branch provides a moderately refactored version of DeepReach to facilitate easier outside research on DeepReach.
 
 ## High-Level Structure
 The code is organized as follows:
-* `dynamics/dynamics.py` defines the dynamics of the system.
+* `dynamics/dynamics.py` defines the example dynamics of the system.
 * `experiments/experiments.py` contains generic training routines.
+* `utils/MPC.py` contains the MPC class for the different reachability cases.
 * `utils/modules.py` contains neural network layers and modules.
 * `utils/dataio.py` loads training and testing data.
 * `utils/diff_operators.py` contains implementations of differential operators.
 * `utils/losses.py` contains loss functions for the different reachability cases.
+* `utils/error_evaluators.py` contains the helper functions for formal verification.
+* `utils/quaternion.py` contains the helper functions for quaternion computation.
 * `run_experiment.py` starts a standard DeepReach experiment run.
+* `run_experiment.sh` contains example commands to train and validate DeepReach with MPC.
 
-## External Tutorial
-Follow along these [tutorial slides](https://docs.google.com/presentation/d/19zxhvZAHgVYDCRpCej2svCw21iRvcxQ0/edit?usp=drive_link&ouid=113852163991034806329&rtpof=true&sd=true) to get started, or continue reading below.
 
 ## Environment Setup
-Create and activate a virtual python environment (env) to manage dependencies:
+Create and activate a virtual python environment (env) in the DeepReach_MPC folder to manage dependencies:
 ```
 python -m venv env
-env\Scripts\activate
 ```
+Activate virtual environment
+```
+source env/bin/activate # Linux user
+env\Scripts\activate # Windows user
+```
+
 Install DeepReach dependencies:
 ```
 pip install -r requirements.txt
@@ -48,48 +44,31 @@ Install the appropriate PyTorch package for your system. For example, for a Wind
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 ```
 
+## External Tutorial
+Follow along these [tutorial slides](https://docs.google.com/presentation/d/1qLU4i1aBQR58G-FiyGb-l9IycMWoJlgq/edit?usp=sharing&ouid=112832011741826436488&rtpof=true&sd=true) to get started, or continue reading below. Currently the tutorial slides include the instruction for writing your own reachability problems, training the network for BRTs, and verifying the BRTs. More tutorials are coming soon.
+
 ## Running a DeepReach Experiment
-`run_experiment.py` implements a standard DeepReach experiment. For example, to learn the value function for the avoid Dubins3D system with parameters `goalR=0.25`, `velocity=0.6`, `omega_max=1.1`, run:
+Here we take the first example from the [tutorial slides](https://docs.google.com/presentation/d/1qLU4i1aBQR58G-FiyGb-l9IycMWoJlgq/edit?usp=sharing&ouid=112832011741826436488&rtpof=true&sd=true), that learns the value function for the avoid Vertical Drone 2D system. Make sure to replace YOUR_WANDB_NAME with your wandb account before running:
 ```
-python run_experiment.py --mode train --experiment_class DeepReach --dynamics_class Dubins3D --experiment_name dubins3d_tutorial_run --minWith target --goalR 0.25 --velocity 0.6 --omega_max 1.1 --angle_alpha_factor 1.2 --set_mode avoid
-```
-Note that the script provides many common training arguments, like `num_epochs` and the option to `pretrain`. Up-to-date, documentation for these different training schemes is lacking; feel free to reach out to the lab for questions. `use_CSL` is an experimental training option (similar in spirit to actor-critic methods) being developed by SIA for improved value function learning. 
-
-## Monitoring a DeepReach Experiment
-Results for the Dubins3D system specified in the above section can be found in this [online WandB project](https://wandb.ai/aklin/DeepReachTutorial).
-We highly recommend users use the `--use_wandb` flag to log training progress to the free cloud-based Weights & Biases AI Developer Platform, where it can be easily viewed and shared.
-
-Throughout training, the training loss curves, value function plots, and model checkpoints are saved locally to `runs/experiment_name/training/summaries` and `runs/experiment_name/training/checkpoints` (and to WandB, if specified).
-
-## Defining a Custom System
-Systems are defined in `dynamics/dynamics.py` and inherit from the abstract `Dynamics` class. At a minimum, users must define:
-* `__init(self, ...)__`, which must call `super().__init__(loss_type, set_mode, state_dim, ...)`
-* `state_test_range(self)`, which specifies the state space that will be visualized in training plots
-* `dsdt(self, state, control, disturbance)`, which implements the forward dynamics
-* `boundary_fn(self, state)`,  which implements the boundary function that implicitly represents the target set
-* `hamiltonian(self, state, dvds)`, which implements the system's hamiltonian
-* `plot_config(self)`, which specifies the state slices and axes visualized in training plots
-
-## Citation
-If you find our work useful in your research, please cite:
-```
-@software{deepreach2024,
-  author = {Lin, Albert and Feng, Zeyuan and Borquez, Javier and Bansal, Somil},
-  title = {{DeepReach Repository}},
-  url = {https://github.com/smlbansal/deepreach},
-  year = {2024}
-}
+python run_experiment.py --mode train --experiment_name VD --dynamics_class VertDrone2D --tMax 1.2 --pretrain --pretrain_iters 1000 --num_epochs 10000 --counter_end 6000 --num_nl 128 --lr 3e-5 --num_iterative_refinement 10 --MPC_batch_size 100 --num_MPC_batches 10 --num_MPC_data_samples 100 --numpoints 10000 --time_till_refinement 0.24 --use_wandb --wandb_project MPC --wandb_name VD --wandb_group VertDrone2D --wandb_entity YOUR_WANDB_NAME
 ```
 
+Note that the script provides many common training arguments, like `num_epochs` and the option to `pretrain`. Please refer to the [tutorial slides](https://docs.google.com/presentation/d/1qLU4i1aBQR58G-FiyGb-l9IycMWoJlgq/edit?usp=sharing&ouid=112832011741826436488&rtpof=true&sd=true) and `run_experiment.py` for more details.
+
+For verifying the learned value function, run:
 ```
-@inproceedings{bansal2020deepreach,
-    author = {Bansal, Somil
-              and Tomlin, Claire},
-    title = {{DeepReach}: A Deep Learning Approach to High-Dimensional Reachability},
-    booktitle = {IEEE International Conference on Robotics and Automation (ICRA)},
-    year={2021}
-}
+python run_experiment.py --mode test --experiment_name VD --checkpoint_toload -1 --data_step run_basic_recovery
 ```
 
-## Contact
-If you have any questions, please feel free to email the authors.
+For visualizing the verification results, run:
+```
+python run_experiment.py --mode test --experiment_name VD --checkpoint_toload -1 --data_step plot_basic_recovery
+```
+The plot will be available at ./runs/VD/basic_BRTs.png.
+
+
+For any question, please feel free to raise an issue. Also note that we may migrate the repo after RSS decision released.
+
+
+## Coming soon
+Forward Reachable Set computation and Reach-Avoid Problem solutions.
--- utils/modules.py
+++ utils/modules.py
@@ -1,32 +1,9 @@
-"""
-MIT License
-
-Copyright (c) 2020 Vincent Sitzmann
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import torch
 from torch import nn
 import numpy as np
 from collections import OrderedDict
 import math
+import torch.nn.functional as F
 
 
 class BatchLinear(nn.Linear):
@@ -40,7 +17,8 @@ class BatchLinear(nn.Linear):
         bias = params.get('bias', None)
         weight = params['weight']
 
-        output = input.matmul(weight.permute(*[i for i in range(len(weight.shape) - 2)], -1, -2))
+        output = input.matmul(weight.permute(
+            *[i for i in range(len(weight.shape) - 2)], -1, -2))
         output += bias.unsqueeze(-2)
         return output
 
@@ -52,6 +30,15 @@ class Sine(nn.Module):
     def forward(self, input):
         # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
         return torch.sin(30 * input)
+
+
+class Saturation(nn.Module):
+    def __init(self):
+        super().__init__()
+
+    def forward(self, input):
+        # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
+        return torch.minimum(torch.maximum(input, torch.zeros_like(input)), torch.ones_like(input))
 
 
 class FCBlock(nn.Module):
@@ -66,13 +53,13 @@ class FCBlock(nn.Module):
 
         # Dictionary that maps nonlinearity name to the respective function, initialization, and, if applicable,
         # special first-layer initialization scheme
-        nls_and_inits = {'sine':(Sine(), sine_init, first_layer_sine_init),
-                         'relu':(nn.ReLU(inplace=True), init_weights_normal, None),
-                         'sigmoid':(nn.Sigmoid(), init_weights_xavier, None),
-                         'tanh':(nn.Tanh(), init_weights_xavier, None),
-                         'selu':(nn.SELU(inplace=True), init_weights_selu, None),
-                         'softplus':(nn.Softplus(), init_weights_normal, None),
-                         'elu':(nn.ELU(inplace=True), init_weights_elu, None)}
+        nls_and_inits = {'sine': (Sine(), sine_init, first_layer_sine_init),
+                         'relu': (nn.ReLU(inplace=True), init_weights_normal, None),
+                         'sigmoid': (nn.Sigmoid(), init_weights_xavier, None),
+                         'tanh': (nn.Tanh(), init_weights_xavier, None),
+                         'selu': (nn.SELU(inplace=True), init_weights_selu, None),
+                         'softplus': (nn.Softplus(), init_weights_normal, None),
+                         'elu': (nn.ELU(inplace=True), init_weights_elu, None)}
 
         nl, nl_weight_init, first_layer_init = nls_and_inits[nonlinearity]
 
@@ -92,7 +79,8 @@ class FCBlock(nn.Module):
             ))
 
         if outermost_linear:
-            self.net.append(nn.Sequential(BatchLinear(hidden_features, out_features)))
+            self.net.append(nn.Sequential(
+                BatchLinear(hidden_features, out_features)))
         else:
             self.net.append(nn.Sequential(
                 BatchLinear(hidden_features, out_features), nl
@@ -102,7 +90,8 @@ class FCBlock(nn.Module):
         if self.weight_init is not None:
             self.net.apply(self.weight_init)
 
-        if first_layer_init is not None: # Apply special initialization to first layer, if applicable.
+        # Apply special initialization to first layer, if applicable.
+        if first_layer_init is not None:
             self.net[0].apply(first_layer_init)
 
     def forward(self, coords, params=None, **kwargs):
@@ -117,11 +106,14 @@ class SingleBVPNet(nn.Module):
     '''A canonical representation network for a BVP.'''
 
     def __init__(self, out_features=1, type='sine', in_features=2,
-                 mode='mlp', hidden_features=256, num_hidden_layers=3, **kwargs):
+                 mode='mlp', hidden_features=256, num_hidden_layers=3,periodic_transform_fn=None, **kwargs):
         super().__init__()
         self.mode = mode
+        self.periodic_transform_fn=periodic_transform_fn
         self.net = FCBlock(in_features=in_features, out_features=out_features, num_hidden_layers=num_hidden_layers,
                            hidden_features=hidden_features, outermost_linear=True, nonlinearity=type)
+        # self.fct = nn.Linear(1, 1)
+
         print(self)
 
     def forward(self, model_input, params=None):
@@ -129,12 +121,83 @@ class SingleBVPNet(nn.Module):
             params = OrderedDict(self.named_parameters())
 
         # Enables us to compute gradients w.r.t. coordinates
-        # TODO: should not need to .clone().detach().requires_grad_(True); instead, use .retain_grad() on input in calling script
-        # otherwise, .detach() removes input from the graph so grad cannot propagate back end-to-end, e.g., percept -> NN -> state estimation (input)
-        coords_org = model_input['coords'].clone().detach().requires_grad_(True)
-        coords = coords_org
+        coords_org = model_input['coords'].clone(
+        ).detach().requires_grad_(True)
+        coords_transformed=self.periodic_transform_fn(coords_org)
+        output = self.net(coords_transformed)
+        return {'model_in': coords_org, 'model_out': output}
 
+class SingleBVPNetEval(nn.Module):
+    '''A canonical representation network for a BVP.'''
+
+    def __init__(self, out_features=1, type='sine', in_features=2,
+                 mode='mlp', hidden_features=256, num_hidden_layers=3, **kwargs):
+        super().__init__()
+        self.mode = mode
+        self.net = FCBlock(in_features=in_features, out_features=out_features, num_hidden_layers=num_hidden_layers,
+                           hidden_features=hidden_features, outermost_linear=True, nonlinearity=type)
+        # self.fct = nn.Linear(1, 1)
+        # print(self)
+
+    def forward(self, model_input, params=None):
+        if params is None:
+            params = OrderedDict(self.named_parameters())
+
+        output = self.net(model_input['coords'])
+        return {'model_in': model_input['coords'], 'model_out': output}
+
+
+class SingleBVPNet2(nn.Module):
+    '''A canonical representation network for a BVP.'''
+
+    def __init__(self, out_features=1, type='sine', in_features=2,
+                 mode='mlp', hidden_features=256, num_hidden_layers=3, **kwargs):
+        super().__init__()
+        self.mode = mode
+        self.net = FCBlock(in_features=in_features, out_features=out_features, num_hidden_layers=num_hidden_layers,
+                           hidden_features=hidden_features, outermost_linear=True, nonlinearity=type)
+        self.fct = nn.Linear(1, 1)
+        print(self)
+
+    def forward(self, model_input, params=None):
+        if params is None:
+            params = OrderedDict(self.named_parameters())
+
+        # Enables us to compute gradients w.r.t. coordinates
+        coords_org = model_input['coords'].clone(
+        ).detach().requires_grad_(True)
+        t_values = self.fct(coords_org[..., [0]])
+        t_values = torch.minimum(torch.maximum(t_values, torch.zeros_like(
+            t_values)), torch.ones_like(t_values))
+        try:
+            coords = torch.cat((t_values, coords_org[..., 1:]), dim=2)
+        except:
+            coords = torch.cat((t_values, coords_org[..., 1:]), dim=1)
         output = self.net(coords)
+        return {'model_in': coords_org, 'model_out': output}
+
+
+class Combined_model(nn.Module):
+    def __init__(self, modelA, modelB):
+        super(Combined_model, self).__init__()
+        self.modelA = modelA
+        self.modelB = modelB
+
+    def forward(self, x, params=None):
+        # coords_org = x['coords'].clone().detach().requires_grad_(True)
+        # v = self.modelA(x)['model_out'].clone().detach().requires_grad_(True)
+        # # print(coords_org.size(),v['model_out'].size())
+        # coords_aug = torch.cat((coords_org,v), dim=2)
+        # result=self.modelB({'coords': coords_aug.cuda()})
+        # return {'model_in': coords_org, 'model_out': result['model_out']}
+        coords_org = x['coords'].clone().detach().requires_grad_(True)
+        v = self.modelA.net(coords_org)
+
+        try:
+            coords_aug = torch.cat((coords_org, v), dim=2)
+        except:
+            coords_aug = torch.cat((coords_org, v), dim=1)
+        output = self.modelB.net(coords_aug)
         return {'model_in': coords_org, 'model_out': output}
 
 
@@ -143,7 +206,8 @@ class SingleBVPNet(nn.Module):
 def init_weights_normal(m):
     if type(m) == BatchLinear or type(m) == nn.Linear:
         if hasattr(m, 'weight'):
-            nn.init.kaiming_normal_(m.weight, a=0.0, nonlinearity='relu', mode='fan_in')
+            nn.init.kaiming_normal_(
+                m.weight, a=0.0, nonlinearity='relu', mode='fan_in')
 
 
 def init_weights_selu(m):
@@ -157,7 +221,8 @@ def init_weights_elu(m):
     if type(m) == BatchLinear or type(m) == nn.Linear:
         if hasattr(m, 'weight'):
             num_input = m.weight.size(-1)
-            nn.init.normal_(m.weight, std=math.sqrt(1.5505188080679277) / math.sqrt(num_input))
+            nn.init.normal_(m.weight, std=math.sqrt(
+                1.5505188080679277) / math.sqrt(num_input))
 
 
 def init_weights_xavier(m):
@@ -171,7 +236,8 @@ def sine_init(m):
         if hasattr(m, 'weight'):
             num_input = m.weight.size(-1)
             # See supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
+            m.weight.uniform_(-np.sqrt(6 / num_input) / 30,
+                              np.sqrt(6 / num_input) / 30)
 
 
 def first_layer_sine_init(m):
